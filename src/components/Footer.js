1f166c56a61d23ee0d21145f22bdb03a9f38988d
Willieaugustine <mulwawillie@gmail.com>
2024-09-18 19:15:37 +0300

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// footer.jsx
+
+import React from 'react'
+import './Footer.css';
+
+function footer() {
+return (
+    <div className='footer'>
+        <h2> © 2024 CrwodFunding. All rights reserved.</h2>
+        </div>
+)
+}
+
+export default footer
